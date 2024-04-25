3658ffebcc398dcd65c133268c58e95c34e4ecc1
RidaOuledhaddou <ouledhaddourida@gmail.com>
2024-04-25 11:22:05 -0700

    AirBnB_clone

--- models/review.py
+++ models/review.py
@@ -3,6 +3,14 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """ DOC DOC DOC """
     place_id = ""
     user_id = ""
     text = ""
+
+
+"""
+    def __init__(self, place_id, user_id):
+        Review.place_id = place_id
+        Review.user_id = user_id
+"""
